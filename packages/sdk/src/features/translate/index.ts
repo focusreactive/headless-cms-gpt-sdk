cd86bfd3768b4e49b6de6538272c2c63a179dcb0
Alex Hramovich <alex.gramovich.1996@gmail.com>
2023-11-03 08:36:21 +0100

    update translation prompt

--- packages/sdk/src/features/translate/index.ts
+++ packages/sdk/src/features/translate/index.ts
@@ -20,12 +20,15 @@ export const translate = async ({
     messages: [
       {
         role: "system",
-        content: `You will be provided with a text, and your task is to translate it into ${targetLanguage}.`,
+        content: `You will be provided with a text, and your task is to translate it into ${targetLanguage}. Act like a native ${targetLanguage} speaker and rephase the text in a way that it sounds natural.`,
       },
       { role: "user", content },
     ],
     model: "gpt-3.5-turbo",
     temperature: 0,
+    top_p: 1,
+    frequency_penalty: 0,
+    presence_penalty: 0,
   });
 
   console.log("chatCompletion", chatCompletion);
