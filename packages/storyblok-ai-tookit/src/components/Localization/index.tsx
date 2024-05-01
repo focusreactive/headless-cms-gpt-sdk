3a7d9a6cc7a49592e5abb9100acc182e0d0f37fa
Sergey Labut <sergeylabut@gmail.com>
2024-05-01 12:45:50 +0200

    Add limit

--- packages/storyblok-ai-tookit/src/components/Localization/index.tsx
+++ packages/storyblok-ai-tookit/src/components/Localization/index.tsx
@@ -3,13 +3,28 @@ import React from 'react'
 import { localizeStory, summariseStory } from '@focus-reactive/storyblok-ai-sdk'
 import LocalizeStoryMode from './modes/Story'
 import { AppDataContext } from '@src/context/AppDataContext'
+import { PLUGIN_ID } from '@src/constants'
+
+async function saveEvent({ spaceId, userId, errorMessage }) {
+  await fetch('/api/usage', {
+    method: 'POST',
+    body: JSON.stringify({
+      eventName: 'fieldLevelTranslation',
+      pluginId: PLUGIN_ID,
+      spaceId,
+      userId,
+      errorMessage,
+    }),
+  })
+}
 
 const Localization = () => {
   const [targetLanguage, setTargetLanguage] = React.useState<string>('')
   const [isLoading, setIsLoading] = React.useState<boolean>(false)
   const [successMessage, setSuccessMessage] = React.useState<string>('')
   const [storySummary, setStorySummary] = React.useState<string>('')
-  const spaceLanguages = React.useContext(AppDataContext)?.languages || []
+  const { languages, spaceId, userId } = React.useContext(AppDataContext)
+  const spaceLanguages = languages || []
 
   const cratePageContext = async () => {
     await summariseStory({
@@ -25,24 +40,38 @@ const Localization = () => {
     setIsLoading(true)
     setSuccessMessage('')
 
-    await cratePageContext()
+    const isUseAllowed = await fetch(`/api/usage?spaceId=${spaceId}`)
 
-    await localizeStory({
-      targetLanguageCode: targetLanguage.replace('-', '_'),
-      targetLanguageName:
-        spaceLanguages.find((lang) => lang.code === targetLanguage)?.name || '',
-      mode: 'update',
-      promptModifier: storySummary
-        ? `Use this text as a context, do not add it to the result translation: "${storySummary}"`
-        : '',
-      cb: () => {
-        setIsLoading(false)
-        setTargetLanguage('')
-        setSuccessMessage(
-          'Success! Change the language to see the localized content.',
-        )
-      },
-    })
+    if (isUseAllowed) {
+      let errorMessage = ''
+
+      try {
+        await cratePageContext()
+        await localizeStory({
+          targetLanguageCode: targetLanguage.replace('-', '_'),
+          targetLanguageName:
+            spaceLanguages.find((lang) => lang.code === targetLanguage)?.name ||
+            '',
+          mode: 'update',
+          promptModifier: storySummary
+            ? `Use this text as a context, do not add it to the result translation: "${storySummary}"`
+            : '',
+          cb: () => {
+            setIsLoading(false)
+            setTargetLanguage('')
+            setSuccessMessage(
+              'Success! Change the language to see the localized content.',
+            )
+          },
+        })
+      } catch (error) {
+        errorMessage = error.message
+      } finally {
+        saveEvent({ spaceId, userId, errorMessage })
+      }
+    } else {
+      setIsLoading(false)
+    }
   }
 
   return (
